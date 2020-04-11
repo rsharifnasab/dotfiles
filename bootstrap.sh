a595dbb57435a3e9304bc7e1f37c7e8fa697bb4a
roozbeh sharifnasab <rsharifnasab@gmail.com>
2020-04-11 23:18:28 +0430

    edited bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,15 +6,15 @@ git pull origin master;
 
 function doIt() {
 	
-        # copy content of this dir to home folder
-        rsync --exclude ".git/" \
-		--exclude "bootstrap.sh" \
-		--exclude "*.md" \
-		--exclude "LICENSE" \
-		-avh --no-perms . ~;
+    # copy content of this dir to home folder
+    rsync --exclude ".git/" \
+        --exclude "bootstrap.sh" \
+	--exclude "*.md" \
+	--exclude "all.sh" \
+	--exclude "LICENSE" \
+	-avh --no-perms . ~;
 
-
-	#source ~/.bash_profile;
+    #source ~/.bash_profile;
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
