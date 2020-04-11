b808d54aa7d98d657bcb71f060f38e8f6c4b538d
roozbeh sharifnasab <rsharifnasab@gmail.com>
2020-04-11 21:02:00 +0430

    edited fish

--- fish/install.sh
+++ fish/install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh 
 
 cp ./fish/config.fish ~/.config/fish/config.fish
-cp -r ./fish/functions ~/.config/fish/functions
+cp -r ./fish/functions ~/.config/fish/
 #source ~/.config/fish/config.fish #idk it really works or not 
