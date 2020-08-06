f8e2206a27dccaf75349653104a9cd63fa47f2f9
roozbeh sharifnasab <rsharifnasab@gmail.com>
2020-08-06 14:37:08 +0430

    added wrodwrap?

--- .doom.d/packages.el
+++ .doom.d/packages.el
@@ -1,10 +1,9 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; $DOOMDIR/packages.el
 
-;; To install a package with Doom you must declare them here and run 'doom sync'
-;; on the command line, then restart Emacs for the changes to take effect -- or
-;; use 'M-x doom/reload'.
-
+;; To install a package with Doom:
+;; 1-declare them here 
+;; 2= and run 'doom sync'
 
 ;; To install SOME-PACKAGE from MELPA, ELPA or emacsmirror:
 ;(package! some-package)
