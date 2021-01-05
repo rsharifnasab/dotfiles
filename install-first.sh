11c0331f58e0abf31355713f68eecc551ed3ffbf
Roozbeh Sharifnasab <rsharifnasab@gmail.com>
2021-01-05 19:02:45 +0330

    added theme.sh

--- install-first.sh
+++ install-first.sh
@@ -4,7 +4,7 @@
 echo "installing requierments"
 yay -S --needed ghc-static cabal-install fd emacs python-pipenv python-pytest python-black python-pyflakes python-rednose python-pytest hlint shellcheck rtags fzf lsd neovim python-pylint-venv python-pynvim clang ctags the_silver_searcher fisher httpie astyle clang-format-static-bin autopep8 gofmt stylish-haskell js-beautify eslint tidy stylelint rustup rust-analyzer xsel
 
-# setup rust toolchain 
+# setup rust toolchain
 rustup install stable
 rustup default stable
 rustup component add clippy
@@ -16,15 +16,28 @@ rustup toolchain add nightly
 rustup component add rust-src
 cargo +nightly install racer # auto complete engine for rust
 
-# clone rust source code
-mkdir -p ~/apps 
+mkdir -p ~/apps
 cd ~/apps || exit
+
+echo "clone rust source code"
 git clone --depth=1 https://github.com/rust-lang/rust.git
+
+
+echo "install tir (time.ir in shell)"
+git clone --depth 1  https://github.com/Pouriya-Jahanbakhsh/tir \
+    && cd tir \
+    && sudo make install\
+    && cd .. \
+    || echo "cannot install tir"
+
+echo "installing theme.sh"
+git clone https://github.com/lemnos/theme.sh || echo "cannot clone theme sh"
+
 cd - || exit
 
 
 #emacs setup
-echo "setupping emacs"
+echo "setup emacs"
 rm -r ~/.emacs.d
 git clone --depth 1 https://github.com/hlissner/doom-emacs ~/.emacs.d
 ~/.emacs.d/bin/doom install
@@ -42,13 +55,13 @@ echo "downloading vimplug for vim"
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-# install vim plugins 
+# install vim plugins
 cp ./.vimrc ~/.vimrc
 vim -c ':PlugInstal'
 
 
 ## neovim
-# install vim plug for nvim 
+# install vim plug for nvim
 echo "downloading vimplug for neovim"
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
@@ -70,11 +83,6 @@ fisher add halostatue/fish-fzf
 echo "setup safe rm"
 npm i -g safe-rm
 
-echo "install tir (time.ir in shell)"
-mkdir -p ~/apps
-cd ~/apps
-git clone --depth 1 --branch 19.10.07 https://github.com/Pouriya-Jahanbakhsh/tir && cd tir
-sudo make install 
 
 
 echo "installing micro"
