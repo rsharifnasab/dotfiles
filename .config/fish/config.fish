4a0f7c3b600abee29e9da88197372e1b3d7ce80e
Roozbeh Sharifnasab <rsharifnasab@gmail.com>
2021-01-08 14:07:44 +0330

    conditional safe-rm to fishrc

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,9 +1,12 @@
-
-# set vim as default text editor 
+# set vim as default text editor
 set -gx EDITOR nvim
 set -gx VISUAL nvim
 #bind \t '__fzf_complete'
-alias rm='/usr/bin/safe-rm'
+
+SAFE_RM='/usr/bin/safe-rm'
+if test -f "$SAFE_RM"; then
+alias rm="$SAFE_RM"
+fi
 
 
 # please clear my terminal
@@ -13,14 +16,12 @@ alias CLEAR='clear'
 alias زمثشق='clear'
 
 
-
 alias em-term='emacsclient -a ""'
 alias em='emacsclient -n -c -a ""'
 alias :q=exit
 
 
-
+alias glog='git log --graph --oneline --decorate --abbrev-commit'
 alias junit='cp -r ~/pro*/*utils/junit_test_runner/* .'
 
-alias glog='git log --graph --oneline --decorate --abbrev-commit'
 
