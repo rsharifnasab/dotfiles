ca96278fa1fef57f8b950478e25c04c38a25d89f
Roozbeh Sharifnasab <rsharifnasab@gmail.com>
2021-01-08 14:17:37 +0330

    some rename

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,38 +4,38 @@ cd "$(dirname "${BASH_SOURCE}")";
 
 git pull origin master;
 
-function doIt() {
-	
+function sync_dotfiles() {
+
     # copy content of this dir to home folder
     echo "copying"
     rsync --exclude ".git/" \
         --exclude "bootstrap.sh" \
         --exclude "install-first.sh" \
-	--exclude "*.md" \
-	--exclude "all.sh" \
-	--exclude "LICENSE" \
-	-avh --no-perms . ~;
+        --exclude "*.md" \
+        --exclude "all.sh" \
+        --exclude "LICENSE" \
+        -avh --no-perms . ~;
 
     #source ~/.config/fish/config.fish
-   
+
     echo "syncing doom"
     ~/.emacs.d/bin/doom -d sync
 
     echo "kill emacs daemon"
     #emacsclient -e 'doom/reload'
     emacsclient -e '(kill-emacs)'
-    
+
     echo "re-open emacs daemon"
     emacs --daemon
 }
 
-if [ "$1" == "--force" -o "$1" == "-f" ]; then
-	doIt;
+if [ "$1" == "--force" ] || [ "$1" == "-f" ]; then
+    doIt;
 else
-	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
-	echo "";
-	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		doIt;
-	fi;
+    read -rp "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
+    echo "";
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        sync_dotfiles;
+    fi;
 fi;
-unset doIt;
+unset sync_dotfiles;
