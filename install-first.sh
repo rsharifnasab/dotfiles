39c564f8eaebe1d1f429abf6c6324d58e0cbb1a6
Roozbeh Sharifnasab <rsharifnasab@gmail.com>
2021-07-06 09:38:23 +0430

    add dig to ubuntu

--- install-first.sh
+++ install-first.sh
@@ -137,7 +137,7 @@ sudo ln -s ~/.emacs.d/bin/doom /usr/bin/doom
 
 # ubuntu specific
 
-sudo apt install python-neovim python3-neovim snapd cmake npm gcc g++ zsh-autosuggestions tldr nnn fzf tree shellcheck xsel httpie clang ctags 
+sudo apt install python-neovim python3-neovim snapd cmake npm gcc g++ zsh-autosuggestions tldr nnn fzf tree shellcheck xsel httpie clang ctags dnsutils
 sudo snap install nvim --classic
 sudo snap install go --classic # newer than apt version
 
