725b79081a3120e02cebae3e78598248077772ff
Roozbeh Sharifnasab <rsharifnasab@gmail.com>
2022-12-22 11:16:27 +0330

    feat(post install) lsof should be installed at first

--- install/post_install.sh
+++ install/post_install.sh
@@ -8,7 +8,7 @@ function pre_install() {
     # install minimum tools to survive next steps!
     sudo pacman -Sy  --noconfirm archlinux-keyring
     sudo pacman -Syu
-    sudo pacman -S --needed --noconfirm base-devel gvim xsel xclip wget curl stow man-pages man
+    sudo pacman -S --needed --noconfirm base-devel gvim xsel xclip wget curl stow man-pages man lsof
 }
 
 function inst() {
