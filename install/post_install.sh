238f8e0f6b72569b8ada74edce937ae2d2769908
Roozbeh Sharifnasab <rsharifnasab@gmail.com>
2023-06-13 20:25:58 +0330

    feat

--- install/post_install.sh
+++ install/post_install.sh
@@ -202,7 +202,7 @@ function python_devel() {
         #mypy --install-types
     )
 
-    inst python-pylint python-black pyright autopep8 pynvim 
+    inst python-pylint python-black pyright autopep8 pynvim
 
     #inst python-pylint-venv python-pipenv python-pytest \
     # python-rednose python-pytest autopep8
