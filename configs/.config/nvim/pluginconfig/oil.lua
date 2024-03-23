0f11e95af1dc65a4a1674df5266bd216e87c2f25
Roozbeh Sharifnasab <rsharifnasab@gmail.com>
2024-03-23 22:38:21 +0330

    feat(plugins) show hidden in oil

--- configs/.config/nvim/pluginconfig/oil.lua
+++ configs/.config/nvim/pluginconfig/oil.lua
@@ -83,7 +83,7 @@ require("oil").setup({
 	use_default_keymaps = true,
 	view_options = {
 		-- Show files and directories that start with "."
-		show_hidden = false,
+		show_hidden = true,
 		-- This function defines what is considered a "hidden" file
 		is_hidden_file = function(name, bufnr)
 			return vim.startswith(name, ".")
