5d6b2927e32930c095457f131ad5c3bbf6408450
Roozbeh Sharifnasab <rsharifnasab@gmail.com>
2024-03-23 22:16:48 +0330

    feat(plugins) conform todo

--- configs/.config/nvim/pluginconfig/conform.lua
+++ configs/.config/nvim/pluginconfig/conform.lua
@@ -120,3 +120,6 @@ end, {
 vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
 
 vim.api.nvim_set_keymap("n", "<leader>l", ":Format<CR>", { noremap = true })
+
+-- TODO: make a custom linter for astyle (c/java)
+--https://github.com/stevearc/conform.nvim/issues/339
