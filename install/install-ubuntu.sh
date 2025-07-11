12aefbc1d1d8da60067b0060b5bef287bb77f5e9
Roozbeh Sharifnasab <rsharifnasab@gmail.com>
2025-07-11 23:12:12 +0330

    feat: add wip ubuntu script

---
+++ install/install-ubuntu.sh
@@ -0,0 +1,102 @@
+sudo apt update
+sudo apt upgrade
+
+sudo apt install vim-gtk3 btop acpi stow \
+    lsd git zsh vlc chromium python-is-python \
+    oathtool npm cargo aria2 curl gnupg \
+    spotify-client zoxide rg silversearcher-ag
+
+mkdir -p ~/.zsh/completions
+
+# kitty
+curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
+
+mkdir -p ~/.local/bin/
+ln -sf ~/.local/kitty.app/bin/kitty ~/.local/kitty.app/bin/kitten ~/.local/bin/
+cp ~/.local/kitty.app/share/applications/kitty.desktop ~/.local/share/applications/
+# If you want to open text files and images in kitty via your file manager also add the kitty-open.desktop file
+cp ~/.local/kitty.app/share/applications/kitty-open.desktop ~/.local/share/applications/
+# Update the paths to the kitty and its icon in the kitty desktop file(s)
+
+sed -i "s|Icon=kitty|Icon=$(readlink -f ~)/.local/kitty.app/share/icons/hicolor/256x256/apps/kitty.png|g" ~/.local/share/applications/kitty*.desktop
+sed -i "s|Exec=kitty|Exec=$(readlink -f ~)/.local/kitty.app/bin/kitty|g" ~/.local/share/applications/kitty*.desktop
+# Make xdg-terminal-exec (and hence desktop environments that support it use kitty)
+echo 'kitty.desktop' > ~/.config/xdg-terminals.list
+
+
+# nerdfont
+curl -fsSL https://raw.githubusercontent.com/getnf/getnf/main/install.sh | bash
+echo "24,25" |  getnf
+
+# neovim
+sudo add-apt-repository ppa:neovim-ppa/unstable
+sudo apt update
+sudo apt install neovim python3-pynvim python3-venv python3-socks python3-requests luarocks
+sudo npm install -g neovim
+apt install ruby ruby-dev npm
+
+gem install neovim
+nvim --headless "+echo has('ruby')" +q # verify
+sudo npm install -g neovim
+
+
+# zed
+curl -f https://zed.dev/install.sh | sh
+
+# docker
+curl -fsSL https://get.docker.com -o /tmp/get-docker.sh
+bash /tmp/get-docker.sh
+
+# teams
+sudo mkdir -p /etc/apt/keyrings
+sudo wget -qO /etc/apt/keyrings/teams-for-linux.asc https://repo.teamsforlinux.de/teams-for-linux.asc
+echo "Types: deb
+URIs: https://repo.teamsforlinux.de/debian/
+Suites: stable
+Components: main
+Signed-By: /etc/apt/keyrings/teams-for-linux.asc
+Architectures: amd64" | sudo tee /etc/apt/sources.list.d/teams-for-linux-packages.sources
+sudo apt update
+sudo apt install teams-for-linux
+
+# install fzf
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
+
+# go latest version
+#curl -sL -o ~/bin/gimme https://raw.githubusercontent.com/travis-ci/gimme/master/gimme
+#chmod +x ~/bin/gimme
+#gimme stable
+
+sudo add-apt-repository ppa:longsleep/golang-backports
+sudo apt update
+sudo apt install golang-go
+
+# kubernetes
+sudo apt-get install -y apt-transport-https ca-certificates curl gnupg
+
+sudo mkdir -p -m 755 /etc/apt/keyrings
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.33/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+sudo chmod 644 /etc/apt/keyrings/kubernetes-apt-keyring.gpg # allow unprivileged APT programs to read this keyring
+
+# This overwrites any existing configuration in /etc/apt/sources.list.d/kubernetes.list
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.33/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo chmod 644 /etc/apt/sources.list.d/kubernetes.list   # helps tools such as command-not-found to work correctly
+sudo apt-get update
+sudo apt-get install -y kubectl
+
+# oc and kubectl
+curl https://mirror.openshift.com/pub/openshift-v4/clients/ocp/latest/openshift-client-linux.tar.gz -o /tmp/oc.tar.gz
+# ex /tmp/oc.tar.gz
+# mv oc kubectl ~/bin/
+
+kubectl completion oc > ~/.zshrc/completions/_kubectl
+
+
+# TODO
+# windows-hello style login: https://github.com/boltgolt/howd
+# install ovsidian from flathub
+# test zed
+# neovim fix
+# atuin history?
+# oc autocomplete
